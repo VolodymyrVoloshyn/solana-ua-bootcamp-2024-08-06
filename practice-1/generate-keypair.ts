0eaf11f334daeb1ead8101633d3682d2b2e82ce5
Volodymyr Voloshyn <voloha13@ukr.net>
2024-08-18 23:30:13 +0300

    lab 2

--- practice-1/generate-keypair.ts
+++ practice-1/generate-keypair.ts
@@ -3,11 +3,11 @@ import { Keypair } from "@solana/web3.js";
 
 const Base58 = require("base-58");
 
-let keyPair = Keypair.generate();
+//let keyPair = Keypair.generate();
 
-console.log("Generated")
-console.log("Public key:", keyPair.publicKey);
-console.log("Secret key:", keyPair.secretKey);
+// console.log("Generated")
+// console.log("Public key:", keyPair.publicKey.toBase58());
+// console.log("Secret key:", keyPair.secretKey);
 
 let privateKey = process.env["SECRET_KEY"];
 
@@ -19,16 +19,42 @@ if (privateKey=== undefined){
 
 const skAsArray = Uint8Array.from(JSON.parse(privateKey));
 
-keyPair= Keypair.fromSecretKey(skAsArray);
+let keyPair= Keypair.fromSecretKey(skAsArray);
 
-console.log("From .env")
-console.log("Public key:", keyPair.publicKey.toBase58());
-console.log("Secret key:", keyPair.secretKey);
+// console.log("From .env")
+// console.log("Public key:", keyPair.publicKey.toBase58());
+// console.log("Secret key:", keyPair.secretKey);
+
+const vovaStr: string = "Vova".toLowerCase();
+
+const startTime = new Date();
+
+console.log(`Started at ${startTime.toString()}`);
+
+while(true){
+    let keyPair2 = Keypair.generate();
+
+    let startStr= keyPair2.publicKey.toBase58().substring(1, 5).toLowerCase();
+
+    //console.log(startStr);
+
+    if(startStr === vovaStr){
+        console.log("Vova Public key:", keyPair.publicKey.toBase58());
+        console.log("Vova Secret key:", keyPair.secretKey);
+
+        break;
+    }
+}
+
+const endTime = new Date();
+const duration = endTime.getTime() - startTime.getTime();
+
+console.log(`Finished at ${endTime}. Duration is ${duration / 1000} seconds`);
+
 
 // let sk= Base58.encode(skAsArray);
 // console.log("Secret key:", sk);
 
 
 console.log("Done")
-
 
---
+++ practice-2/create-token-mint.ts
@@ -0,0 +1,38 @@
+import "dotenv/config";
+import {
+    getExplorerLink
+}from "@solana-developers/helpers";
+import {
+    Keypair,
+    clusterApiUrl,
+    Connection
+} from "@solana/web3.js";
+import { createMint } from "@solana/spl-token";
+
+let privateKey = process.env["SECRET_KEY"];
+
+if(privateKey === undefined){
+    console.log("Can't get private key");
+    process.exit(1);
+}
+
+const pKeyAsArray= Uint8Array.from(JSON.parse(privateKey));
+const sender = Keypair.fromSecretKey(pKeyAsArray);
+
+//console.log(sender.publicKey);
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+const tokenMint = await createMint(
+    connection,
+    sender,
+    sender.publicKey,
+    null,
+    2
+);
+
+const link= getExplorerLink("address", tokenMint.toString(), "devnet");
+
+console.log(`token mint: ${link}`);
+
+// token mint: https://explorer.solana.com/address/jXVfZCA5f2AwQeUmVEkk3bEUDn3miq6Pb1LZvNusxpi?cluster=devnet
