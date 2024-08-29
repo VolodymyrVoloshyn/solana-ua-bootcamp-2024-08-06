78f3537aeea467aad4cf734b1b9cc208fc17c3c9
Volodymyr Voloshyn <voloha13@ukr.net>
2024-08-29 22:37:57 +0300

    added favorites

--- practice-1/getPk.ts
+++ practice-1/getPk.ts
@@ -19,6 +19,8 @@ if (privateKey=== undefined){
 
 const skAsArray = Uint8Array.from(JSON.parse(privateKey));
 
+//const skAsArray = Uint8Array.from(JSON.parse("[28,104,185,88,158,119,100,107,56,9,93,214,47,248,122,244,5,148,182,157,144,97,126,139,238,113,72,3,24,98,246,167,31,233,245,187,233,114,173,146,31,245,182,3,227,161,197,64,177,99,169,91,122,206,53,173,18,243,186,3,136,149,102,199]"));
+
 let keyPair= Keypair.fromSecretKey(skAsArray);
 
 console.log("From .env")
